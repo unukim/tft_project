3fc34b6073d01103682ec16c0efdd8d98f6da5de
Phicks <mineran2003@gmail.com>
2024-07-07 13:13:12 +0200

    update API

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from riot import RIOT
 if __name__ == "__main__":
     
     riot = RIOT()
+    
     riot.getLeague("RANKED_TFT")
+    
     
--- riot.py
+++ riot.py
@@ -5,14 +5,30 @@ from helpers import get_api_key
 
 class RIOT:
     def __init__(self):
+        """
+        Initializes the RIOT class with an API key and the base URL for the Riot Games API.
+        The API key is retrieved from a file named "key.txt".
+        """
+        
         self.api_key = get_api_key("key.txt")
         self.base_url = "https://vn2.api.riotgames.com/tft/"
 
 
     
-    def getLeague(self, queue):
+    def getLeague(self, queue = "RANKED_TFT"):
+        """
+        Retrieves the league data for a given queue from the Riot Games API.
+
+        Parameters:
+        queue (str): The queue type (e.g., default to "RANKED_TFT") for which to retrieve league data.
+
+        Returns:
+        None. Prints the retrieved data if the request is successful, or an error message if it fails.
+        """
+        
         url = self.base_url + f"league/v1/challenger?queue={queue}&api_key=" + self.api_key
         try:
+            print(url)
             response = requests.get(url)
             
             if response.status_code == 200:
@@ -23,3 +39,23 @@ class RIOT:
         
         except Exception as e:
             print(f"Error executed API request {e}")
+    
+    
+    def get(self, url):
+        """
+        Makes a GET request to the specified URL and prints the retrieved data if the request is successful.
+
+        Parameters:
+        url (str): The URL to which the GET request is made.
+
+        Returns:
+        None. Prints the retrieved data if the request is successful, or an error message if it fails.
+        """
+        
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+        else:
+            print(f"Failed to retrieve data: {response.status_code}")
